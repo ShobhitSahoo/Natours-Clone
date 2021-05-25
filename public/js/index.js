ba999713cdc01bac8d67ee1fee6c1e3550cc96e9
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-26 01:06:15 +0530

    Updating users data

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill'
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM elements create and check if it exists
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 if(mapBox) {
     const locations =JSON.parse(document.getElementById('map').dataset.locations);
@@ -22,3 +24,12 @@ if(loginForm) {
 }
 
 if(logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if(userDataForm) {
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateData(name, email);
+    })
+}
