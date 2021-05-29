00e747adb65359765492787e970b80cf8c2e212d
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-29 19:55:28 +0530

    Fixed Bug

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,7 +1,7 @@
 const { promisify } = require('util');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
-const User = require('./../models/userModel');
+const User = require('./../Models/userModel');
 const catchAsync = require('./../Utils/catchAsync');
 const AppError = require('./../utils/appError');
 const Email = require('./../utils/email');
