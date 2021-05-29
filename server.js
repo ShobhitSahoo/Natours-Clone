d7c6b6f7d9770a0af32042bc86abf270f08adc33
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-29 19:36:13 +0530

    Updated files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ process.on('uncaughtException', err => {
 
 dotenv.config({path: './config.env'})
 
-const app = require('./app');
+const app = require('app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
