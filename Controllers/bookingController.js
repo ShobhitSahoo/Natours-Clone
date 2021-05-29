ecc41a19073c836542a4c24abfa16c2c8bc933b3
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-30 04:42:32 +0530

    QnA Fix

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -18,16 +18,31 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourID,
+        mode: 'payment',
         line_items: [
-            {
+          {
+            quantity: 1,
+            price_data: {
+              currency: 'usd',
+              unit_amount: tour.price * 100,
+              product_data: {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
                 images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
-                amount: tour.price * 100,
-                currency: 'usd',
-                quantity: 1
-            }
-        ]
+              },
+            },
+          },
+        ],
+        // line_items: [
+        //     {
+        //         name: `${tour.name} Tour`,
+        //         description: tour.summary,
+        //         images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+        //         amount: tour.price * 100,
+        //         currency: 'usd',
+        //         quantity: 1
+        //     }
+        // ]
     })
 
     // 3) Create session as response
@@ -38,13 +53,20 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 });
 
-const createBookingCheckout = async session => {
+// const createBookingCheckout = async session => {
+//     const tour = session.client_reference_id;
+//     const user = (await User.findOne({ email: session.customer_email })).id;
+//     const price = session.display_items[0].amount / 100;
+//     await Booking.create({ tour, user, price });
+//   };
+  
+const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.display_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
-  };
-  
+};
+
   exports.webhookCheckout = (req, res, next) => {
     const signature = req.headers['stripe-signature'];
   
