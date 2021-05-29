1303436053eb5d77bc3642374d8fec851c0ae57b
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-30 05:07:09 +0530

    QnA Fix

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -53,19 +53,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 });
 
+const createBookingCheckout = async session => {
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.display_items[0].amount / 100;
+    await Booking.create({ tour, user, price });
+  };
+  
 // const createBookingCheckout = async session => {
 //     const tour = session.client_reference_id;
 //     const user = (await User.findOne({ email: session.customer_email })).id;
-//     const price = session.display_items[0].amount / 100;
+//     const price = session.amount_total / 100;
 //     await Booking.create({ tour, user, price });
-//   };
-  
-const createBookingCheckout = async (session) => {
-    const tour = session.client_reference_id;
-    const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.amount_total / 100;
-    await Booking.create({ tour, user, price });
-};
+// };
 
   exports.webhookCheckout = (req, res, next) => {
     const signature = req.headers['stripe-signature'];
@@ -75,7 +75,7 @@ const createBookingCheckout = async (session) => {
       event = stripe.webhooks.constructEvent(
         req.body,
         signature,
-        process.env.STRIPE_WEBHOOK_SECRET
+        "whsec_9keOTXtfE7wdVUUgqKmvmNPTZ6vqo5Cc"
       );
     } catch (err) {
       return res.status(400).send(`Webhook error: ${err.message}`);
