f483b848167204216cdba073931353444022b2be
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-31 11:14:46 +0530

    Fixed payment issue

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(DB, {
 }).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
+const server = app.listen(3000, () => console.log(`App listening on port ${port}!`));
 
 process.on('unhandledRejection', err => {
     console.log('UNHANDLED REJECTION 🤯 Shutting Down....');
