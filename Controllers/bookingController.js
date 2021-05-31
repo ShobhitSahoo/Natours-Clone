24424d5d9e96a68099bb6f4a6e4e0dc12de71931
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-05-31 13:54:28 +0530

    Minor fixes

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -12,15 +12,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // 2) Create checkout session
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+        success_url: `https://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+        cancel_url: `https://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourID,
         line_items: [
             {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
-                images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                images: [`https://${req.get('host')}/img/tours/${tour.imageCover}`],
                 amount: tour.price * 100,
                 currency: 'inr',
                 quantity: 1
